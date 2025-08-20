7e90fe042fa09a2acd7b8dd439f7a51b66d4a832
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-08-20 19:06:25 +0500

    Update links.js

--- src/links.js
+++ src/links.js
@@ -1,6 +1,5 @@
-const LINKS = {
+{
   "example": "https://example.com/",
   "github": "https://github.com/",
-  "google": "https://google.com/",
-  "sdjgkjsdk": "https://harys.is-a.dev/"
-};
+  "google": "https://google.com/"
+}
